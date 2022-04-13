bdd69c439d1f5e5cad7d87a600c495a7ffdbcf54
keddib <keddib@student.1337.ma>
2022-04-13 23:09:16 +0000

    controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', [TaskController::class, 'index'])->name('task');
+Route::get('/', [HomeController::class, 'create'])->name('Home');
+Route::get('/comment', [CommentController::class, 'store'])->name('Store');
