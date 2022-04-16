dee62f7ee52709fd3144710dedd9240c09a328ec
keddib <keddib@student.1337.ma>
2022-04-16 05:43:18 +0000

    footer and header for all pages

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,7 @@
-<div class="footer py-2">
-	<div class="container m-auto ">
-		<div class="wrapper flex justify-around w-4/12 m-auto">
-			<p class="wordwide">wordwide</p>
-			<p class="wordwide">wordwide</p>
-			<p class="wordwide">wordwide</p>
-			<p class="wordwide">wordwide</p>
+<div class="footer bg-white">
+	<div class="container m-auto">
+		<div class="wrapper flex justify-around w-4/12 m-auto ">
+			<p class="wordwide">created with love by @keddib</p>
 		</div>
 	</div>
 </div>
