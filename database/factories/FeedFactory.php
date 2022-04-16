e3fa80eff60e516e742833a25f5464cdd2339f26
keddib <keddib@student.1337.ma>
2022-04-16 06:24:24 +0000

    create factory for feeds to generate random name and text

---
+++ database/factories/FeedFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Feed>
+ */
+class FeedFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nickname' => $this->faker->name(),
+            'text' => $this->faker->text(),
+            // 'remember_token' => Str::random(10),
+        ];
+    }
+}
